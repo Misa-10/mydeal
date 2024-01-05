906e53960e631351f19635d6f4e3d989ff814e88
Misa <q.brenas@gmail.com>
2024-01-05 15:59:48 +0100

    add cdn swagger to fix it on vercel

--- API/index.js
+++ API/index.js
@@ -8,6 +8,8 @@ const path = require("path"); // Import the 'path' module
 const usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 const dealsRouter = require("./routes/deals");
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 dotenv.config();
 
@@ -17,7 +19,11 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL })
+);
 app.use("", usersRouter);
 app.use("", authRouter);
 app.use("", dealsRouter);
